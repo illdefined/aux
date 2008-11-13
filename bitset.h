fb0a6de2ab07cba834ab9fb6ee57a324056a2875
Mikael Voss <mikael@koeln.ccc.de>
2008-11-13 09:28:18 +0100

    Use new macros to simplify bitset operations code.

--- bitset.h
+++ bitset.h
@@ -3,17 +3,18 @@
 #define AUX_BITSET_H
 
 #include "types.h"
+#include "oprtr.h"
 
 static void bitset_set(word *bs, word idx) {
-	bs[idx / (sizeof word * 8)] |= WORD_C(1) << idx % (sizeof word * 8);
+	bs[idx / bits(word)] |= WORD_C(1) << idx % bits(word);
 }
 
 static void bitset_clr(word *bs, word idx) {
-	bs[idx / (sizeof word * 8)] &= ~(WORD_C(1) << idx % (sizeof word * 8));
+	bs[idx / bits(word)] &= ~(WORD_C(1) << idx % bits(word));
 }
 
 static bool bitset_tst(const word *bs, word idx) {
-	return bs[idx / (sizeof word * 8)] & (WORD_C(1) << idx % (sizeof word * 8));
+	return bs[idx / bits(word)] & (WORD_C(1) << idx % bits(word));
 }
 
 #endif
