73e9f8c8ee623e4de48850fa8008587acd2e6ba6
Mikael Voss <mikael@koeln.ccc.de>
2008-11-13 09:27:04 +0100

    Added exchange and bitwise rotation macros.

---
+++ oprtr.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef OPRTR_H
+#define OPRTR_H
+
+#define bits(a) (sizeof (a) * 8)
+
+#define xchg(a, b) if (&a != &b) { \
+	a ^= b; \
+	b ^= a; \
+	a ^= b; \
+}
+
+#define rotl(value, shift) \
+	(value << (shift & (bits(value) - 1) | (value >> (bits(value) - (shift & (bits(value) - 1))))
+
+#define rotr(value, shift) \
+	(value >> (shift & (bits(value) - 1) | (value << (bits(value) - (shift & (bits(value) - 1))))
+
+#endif
