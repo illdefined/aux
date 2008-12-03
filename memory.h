14aa2a0700cb3b29f169cde832a47fdda7cba45a
Mikael Voss <mikael@koeln.ccc.de>
2008-12-03 21:44:30 +0100

    Modified memory.h to use types from types.h.

--- memory.h
+++ memory.h
@@ -2,10 +2,17 @@
 #ifndef AUX_MEMORY_H
 #define AUX_MEMORY_H
 
+#define _BSD_SOURCE
+
 #include <stdint.h>
 #include <sys/mman.h>
 
-#define align(size, alignment) (((uintptr_t) (size) + (uintptr_t) (alignment) - 1ul) & ~((uintptr_t) (alignment) - 1ul))
-#define alloc(size) mmap((void *) 0, (size), PROT_READ | PROT_WRITE, MAP_ANON, -1, 0)
+#include "types.h"
+
+#define align(size, alignment) \
+	(((word) (size) + (word) (alignment) - WORD_C(1)) & ~((word) (alignment) - WORD_C(1)))
+
+#define alloc(size) \
+	mmap((void *) 0, (size), PROT_READ | PROT_WRITE, MAP_ANON, -1, 0)
 
 #endif
